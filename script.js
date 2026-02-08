9fb2342d04c74fab319b94685357254c0212633c
shtwet <sht.wetio@gmail.com>
2026-02-08 20:22:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,28 +10,7 @@ letter.classList.add("center");
 let isExpanded = false;
 
 letter.addEventListener("click", (e) => {
- let distanceMoved = 0;
-  const step = 2;
-  const targetDistance = 50;
-
-  const moveInterval = setInterval(() => {
-    if (distanceMoved >= targetDistance) {
-      clearInterval(moveInterval);
-
-      // 2. Box auf fast die ganze Bildschirmhöhe vergrößern
-      const screenHeight = window.innerHeight;
-      const screenWidth = window.innerWidth;
-
-      letter.style.width = (screenWidth * 0.9) + 'px'; // 90% der Bildschirmbreite
-      letter.style.height = (screenHeight * 0.9) + 'px'; // 90% der Bildschirmhöhe
-      letter.style.left = (screenWidth * 0.05) + 'px';  // zentrieren
-      letter.style.top = (screenHeight * 0.05) + 'px';  // zentrieren
-    }
-
-    let currentTop = parseInt(letter.style.top);
-    letter.style.top = (currentTop - step) + 'px';
-    distanceMoved += step;
-  }, 20);
+  letter.style.width = (screenWidth * 0.9) + 'px';
 });
 
 
@@ -39,6 +18,7 @@ letter.addEventListener("click", (e) => {
 document.querySelector("#openEnvelope").addEventListener("click", () => {
   document.querySelector(".envelope").classList.add("active");
 });
+
 
 
 
